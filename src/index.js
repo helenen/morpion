56de8645531a9483099d53b5d90cea5c2889d72b
helenen <helene.niassy@gmail.com>
2019-04-11 17:19:35 +0200

    feat: caseValue

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ export function generateBoard() {
   return grid;
 }
 
-export function playAPawn(board, row, column, gamerOne) {
-  board[row][column] = gamerOne;
+export function playAPawn(board, row, column, playerOne) {
+  const value = playerOne ? 'X' : 'O';
+  const caseValue = board[row][column] !== '' ?  : value ;  
+  board[row][column] = caseValue;
+  console.log(board);
   return board;
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,22 +1,37 @@
 import { generateBoard, playAPawn } from "./index";
 
 describe("Should have a morpion grid", () => {
+  const res = generateBoard();
   test("should return a gameboard of 3 rows", () => {
-    const res = generateBoard();
     expect(res.length).toEqual(3);
   });
   test("should return an empty gameboard of 3 column * 3 row", () => {
-    const res = generateBoard();
-    expect(res).toEqual([["", "", ""], ["", "", ""], ["", "", ""]]);
+    expect(res).toEqual([['', '', ''], ['', '', ''], ['', '', '']]);
   });
 });
 describe("Should play a pawn", () => {
-  test("should return board modified", () => {
-    const board = generateBoard();
-    expect(playAPawn(board, 0, 0, "X")).toEqual([
-      ["X", "", ""],
-      ["", "", ""],
-      ["", "", ""]
+  const board = generateBoard();
+
+  test("should return board modified with playerOne", () => {    
+    expect(playAPawn(board,0,0,true)).toEqual([
+      ['X', '', ''],
+      ['', '', ''],
+      ['', '', '']
     ]);
   });
+  test("should return board modified with playerTwo", () =>{
+    expect(playAPawn(board, 1, 0,false)).toEqual([
+      ['X', '', ''],
+      ['O', '', ''],
+      ['', '', '']
+    ]);
+  });
+  test("should don't modify a pawn already played",() => {
+    expect(playAPawn(board, 1, 0, true)).toEqual([
+      ['X', '', ''],
+      ['O', '', ''],
+      ['', '', '']
+    ]);
+  });
+  
 });
