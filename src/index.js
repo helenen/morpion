f33546fbe0c569896bc1f48a15397c61d8f05eda
helenen <helene.niassy@gmail.com>
2019-04-25 17:11:30 +0200

    feat: continue winningVertical

--- src/index.js
+++ src/index.js
@@ -32,29 +32,35 @@ export function winningHorizonal(board, player) {
   return false
 }
 export function winningDiagonal(board, player) {
-  for (let i = 0, j = 0; i < board.length; i++ , j--) {
-    board[i][i] === player ? true : false;
-    console.log("pilou", board);
-
+  if (board[0][0] === player && board[1][1] === player && board[2][2]) {
+    return true
+  } else if (board[0][2] === player && board[1][1] === player && board[2][0]) {
+    return true;
   }
-  // if (board[0][0] === player && board[1][1] === player && board[2][2]) {
-  //   return true
-  // } else if (board[0][2] === player && board[1][1] === player && board[2][0]) {
-  //   return true;
-  // }
 
 }
 export function winningVertical(board, player) {
-
-  if (board[0][0] === player && board[1][0] === player && board[2][0] === player) {
-    return true
-  } else if (board[0][1] === player && board[1][1] === player && board[2][1] === player) {
-    return true
-  } else if (board[0][3] === player && board[1][3] === player && board[2][3] === player) {
-    return true
+  let count = 0
+  for (let i = 0; i < board.length; i++) {
+    for (let y = 0; y < board[i].length; y++) {
+      if (board[i][y] === player) {
+        count += 1
+      }
+    } if (count === 3) {
+      return true
+    }
   }
-  return false;
+  console.log(count)
+  return false
 }
+// if (board[0][0] === player && board[1][0] === player && board[2][0] === player) {
+//   return true
+// } else if (board[0][1] === player && board[1][1] === player && board[2][1] === player) {
+//   return true
+// } else if (board[0][3] === player && board[1][3] === player && board[2][3] === player) {
+//   return true
+// }
+
 
 export function winningGame(board, player) {
   if (winningHorizonal(board, player) || winningVertical(board, player) || winningDiagonal(board, player)) {
