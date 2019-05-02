706d14eb654b1856e32424736e420abcdc2f973b
helenen <helene.niassy@gmail.com>
2019-05-02 11:02:46 +0200

    feat: delete old code

--- src/index.js
+++ src/index.js
@@ -54,18 +54,8 @@ export function winningVertical(board, player) {
       return true;
     }
   }
-  console.log(count);
   return false;
 }
-
-// if (board[0][0] === player && board[1][0] === player && board[2][0] === player) {
-//   return true
-// } else if (board[0][1] === player && board[1][1] === player && board[2][1] === player) {
-//   return true
-// } else if (board[0][3] === player && board[1][3] === player && board[2][3] === player) {
-//   return true
-// }
-
 export function winningGame(board, player) {
   if (
     winningHorizonal(board, player) ||
