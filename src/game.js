60bbd1bd92482a29b864c832822eae13ead37444
helenen <helene.niassy@gmail.com>
2019-06-11 23:19:23 +0200

    winning vertical

--- src/game.js
+++ src/game.js
@@ -2,13 +2,11 @@
 let player = "X";
 
 export function initializeGame() {
-  const board = generateBoard()
-  const grid = Array.from(document.getElementsByClassName("box"))
+  const board = generateBoard();
+  const grid = Array.from(document.getElementsByClassName("box"));
   grid.map((cell, index) => {
-    cell.addEventListener("click", playAPawn.bind(null, board, index))
-
-  })
-
+    cell.addEventListener("click", playAPawn.bind(null, board, index));
+  });
 }
 
 export function generateBoard() {
@@ -26,57 +24,57 @@ function getPosition(index) {
       return {
         row: 0,
         column: 0
-      }
+      };
     },
     1: () => {
       return {
         row: 1,
         column: 0
-      }
+      };
     },
     2: () => {
       return {
         row: 2,
         column: 0
-      }
+      };
     },
     3: () => {
       return {
         row: 0,
         column: 1
-      }
+      };
     },
     4: () => {
       return {
         row: 1,
         column: 1
-      }
+      };
     },
     5: () => {
       return {
         row: 2,
         column: 1
-      }
+      };
     },
     6: () => {
       return {
         row: 0,
         column: 2
-      }
+      };
     },
     7: () => {
       return {
         row: 1,
         column: 2
-      }
+      };
     },
     8: () => {
       return {
         row: 2,
         column: 2
-      }
+      };
     }
-  }
+  };
 
   const coordinate = coordinates[index];
   return coordinate();
@@ -91,26 +89,26 @@ export function playAPawn(board, index) {
   const coordinates = getPosition(index);
 
   // Get the DOM node in which we want to play
-  const boxGrid = document.getElementById(`box${index}`)
+  const boxGrid = document.getElementById(`box${index}`);
 
   // Play the pawn
   if (board[coordinates.row][coordinates.column] === "") {
     boxGrid.innerHTML = player;
-    board[coordinates.row][coordinates.column] = player
+    board[coordinates.row][coordinates.column] = player;
   }
 
   // Change the player's turn
   if (player === "X") {
-    player = "O"
+    player = "O";
   } else {
-    player = "X"
+    player = "X";
   }
 
   // Check if there is a winner
-  const message = winningGame(board, player)
-  console.log(board, message, 'message')
+  const message = winningGame(board, player);
+  console.log(board, "message");
 
-  return board
+  return board;
 }
 
 export function winningHorizonal(board, player) {
@@ -150,20 +148,23 @@ export function winningVertical(board, player) {
   //parcourir les tableaux
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[i]; j++) {
-      if (board[i][j] === player && count <= 0) {
-        count += 1
-        col = j
+      if (board[i][j] === player && count < 0) {
+        count += 1;
+        col = j;
       } else if (count > 0 && board[i][col] === player) {
-        count += 1
+        count += 1;
+      } else if (board[i][col] === "") {
+        count = 0;
 
+        break;
       }
+      console.log(count, "count");
     }
   }
 }
 
 // TODO: Fix this function
 export function winningGame(board, player) {
-
   let horizontal = winningHorizonal(board, player);
   let vertical = winningVertical(board, player);
   let diagonal = winningDiagonal(board, player);
@@ -188,11 +189,11 @@ export function winningGame(board, player) {
  * And return the new board
  */
 export function aiPlayAPawn(board, player, rowSeed, columnSeed) {
-  const row = Math.floor(rowSeed() * 3)
-  const column = Math.floor(columnSeed() * 3)
+  const row = Math.floor(rowSeed() * 3);
+  const column = Math.floor(columnSeed() * 3);
   const result = playAPawn(board, row, column, player);
 
-  return result
+  return result;
 }
 
 /* console.log(aiPlayAPawn([["X", "", ""], ["", "", ""], ["", "", ""]], "O", () => 0.9, () => 0.5)); */
