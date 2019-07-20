c048447e4cdda17c0d7c07c38acd9796b0e83a7b
helenen <helene.niassy@gmail.com>
2019-07-21 01:40:43 +0200

    add id

--- src/index.js
+++ src/index.js
@@ -1,100 +1,102 @@
-import { generateBoard, playPawn, winner, aiPlayPawn } from './scripts/game2.js';
-
-
+import {
+  generateBoard,
+  playPawn,
+  winner,
+  aiPlayPawn
+} from "./scripts/game2.js";
 
 function generateInterface(board) {
+  const boardContainer = document.createElement("DIV");
+  boardContainer.setAttribute("class", "container");
+  document.body.appendChild(boardContainer);
 
-    const boardContainer = document.createElement("DIV");
-    boardContainer.setAttribute('class', 'container');
-    document.body.appendChild(boardContainer);
+  for (let i = 0; i < board.length; i++) {
+    const column = document.createElement("div");
+    column.setAttribute("class", "column");
+    boardContainer.appendChild(column);
 
-    for (let i = 0; i < board.length; i++) {
-        const column = document.createElement("div");
-        column.setAttribute('class', 'column');
-        boardContainer.appendChild(column);
+    for (let j = 0; j < board[i].length; j++) {
+      const box = document.createElement("div");
+      box.setAttribute("class", "box");
+      box.setAttribute("value", "null");
 
-        for (let j = 0; j < board[i].length; j++) {
-            const box = document.createElement("div");
-            box.setAttribute('class', 'box');
-            column.appendChild(box);
-        }
+      column.appendChild(box);
     }
-
+  }
 }
 
-generateInterface(generateBoard())
+generateInterface(generateBoard());
 
 function playPawnDom(board) {
+  let box = Array.from(document.getElementsByClassName("box"));
 
-    let box = Array.from(document.getElementsByClassName('box'))
+  box.map((e, index) => {
+    e.addEventListener(
+      "click",
+      () => {
+        //place pawn in board functionnal
+        playPawn(board, index, "X");
 
-    box.map((e, index) => {
-        e.addEventListener("click", () => {
-            //place pawn in board functionnal
-            playPawn(board, index, "X");
+        //display player in DOM
+        //e.innerHTML = "X";
 
-            //display player in DOM
-            e.innerHTML = "X"
+        //check if there is a winner and display a modal
+        if (winner !== null) {
+          setTimeout(() => modal(), 500);
+          return;
+        }
 
-            //check if there is a winner and display a modal
-            if (winner !== null) {
-                setTimeout(() => modal(), 500);
-                return
-            }
-
-            setTimeout(() => {
-                aiPlayPawn(board, "O", Math.random());
-            }, 400);
-
-        })
-    });
+        setTimeout(() => {
+          aiPlayPawn(board, "O", Math.random());
+        }, 400);
+      },
+      true
+    );
+  });
 }
 
-playPawnDom(generateBoard())
+playPawnDom(generateBoard());
 
 /** display a modal with winner's name
  *
  */
 export function modal() {
-    //create div with class modal-container
-    const modalContainer = document.createElement("DIV");
-    modalContainer.setAttribute('class', 'modal-container');
+  //create div with class modal-container
+  const modalContainer = document.createElement("DIV");
+  modalContainer.setAttribute("class", "modal-container");
 
-    //create P tag
-    const modalText = document.createElement("P");
+  //create P tag
+  const modalText = document.createElement("P");
 
-    //create div with class modal
-    const modal = document.createElement("DIV");
-    modal.setAttribute('class', 'modal');
+  //create div with class modal
+  const modal = document.createElement("DIV");
+  modal.setAttribute("class", "modal");
 
-    //create reload button
-    const reloadButton = document.createElement("BUTTON");
-    reloadButton.setAttribute('class', 'reload-button');
+  //create reload button
+  const reloadButton = document.createElement("BUTTON");
+  reloadButton.setAttribute("class", "reload-button");
 
-    //create close button
-    const closeButton = document.createElement("BUTTON");
-    closeButton.setAttribute('class', 'close');
+  //create close button
+  const closeButton = document.createElement("BUTTON");
+  closeButton.setAttribute("class", "close");
 
+  //nest elements in DOM
+  document.body.appendChild(modal);
+  modal.appendChild(modalContainer);
+  modalContainer.appendChild(modalText);
+  modalContainer.appendChild(reloadButton);
+  modalContainer.appendChild(closeButton);
 
-    //nest elements in DOM
-    document.body.appendChild(modal);
-    modal.appendChild(modalContainer);
-    modalContainer.appendChild(modalText);
-    modalContainer.appendChild(reloadButton);
-    modalContainer.appendChild(closeButton);
+  //display words
+  modalText.innerHTML = "Tu as gagné " + winner;
+  reloadButton.innerHTML = "recommencer";
+  closeButton.innerHTML = "&times";
 
-    //display words
-    modalText.innerHTML = "Tu as gagné " + winner;
-    reloadButton.innerHTML = "recommencer";
-    closeButton.innerHTML = "&times";
+  //refresh window
+  reloadButton.onclick = () => window.location.reload();
 
-    //refresh window
-    reloadButton.onclick = () => window.location.reload();
-
-    //remove modal
-    closeButton.onclick = function () {
-        modal.style.display = "none";
-    }
-
-
+  //remove modal
+  closeButton.onclick = function() {
+    modal.style.display = "none";
+  };
 }
--- src/scripts/game2.js
+++ src/scripts/game2.js
@@ -1,113 +1,108 @@
-import { winningGame } from './winning2.js'
+import { winningGame } from "./winning2.js";
+
 //winner
 export let winner = "";
 
 // create a board
 export function generateBoard() {
-    let board = new Array(
-        new Array("", "", ""),
-        new Array("", "", ""),
-        new Array("", "", "")
-    );
-    return board;
+  let board = new Array(
+    new Array("", "", ""),
+    new Array("", "", ""),
+    new Array("", "", "")
+  );
+  return board;
 }
 
 function getPosition(index) {
-    const coordinates = {
-        0: {
-            row: 0,
-            column: 0
-        },
-        1: {
-            row: 1,
-            column: 0
-        },
-        2: {
-            row: 2,
-            column: 0
-        },
-        3: {
-            row: 0,
-            column: 1
+  const coordinates = {
+    0: {
+      row: 0,
+      column: 0
+    },
+    1: {
+      row: 1,
+      column: 0
+    },
+    2: {
+      row: 2,
+      column: 0
+    },
+    3: {
+      row: 0,
+      column: 1
+    },
+    4: {
+      row: 1,
+      column: 1
+    },
+    5: {
+      row: 2,
+      column: 1
+    },
+    6: {
+      row: 0,
+      column: 2
+    },
+    7: {
+      row: 1,
+      column: 2
+    },
+    8: {
+      row: 2,
+      column: 2
+    }
+  };
 
-        },
-        4: {
-            row: 1,
-            column: 1
-
-        },
-        5: {
-            row: 2,
-            column: 1
-
-        },
-        6: {
-            row: 0,
-            column: 2
-
-        },
-        7: {
-            row: 1,
-            column: 2
-
-        },
-        8: {
-            row: 2,
-            column: 2
-
-        }
-    };
-
-    return coordinates[index];
-
+  return coordinates[index];
 }
 
 // play a pawn in board
 export function playPawn(board, index, player) {
-    let coordinates = getPosition(index);
+  let box = document.getElementsByClassName("box");
+  for (var i = 0; i < box.length; ++i) {
+    box[i].setAttribute("id", "123456789".charAt(i));
+  }
 
-    // put a X or O when there is a empty string
-    if (board[coordinates.row][coordinates.column] === "") {
-        board[coordinates.row][coordinates.column] = player
+  console.log(box, "box");
+  let coordinates = getPosition(index);
 
-    } else {
-        "error: can't play when a pawn is already played"
-    }
-    // Check if there is a winner
-    winner = winningGame(board, player);
+  // put a X or O when there is a empty string
+  if (board[coordinates.row][coordinates.column] === "") {
+    box[id].innerHTML = player;
+  } else {
+    ("error: can't play when a pawn is already played");
+  }
+  // Check if there is a winner
+  winner = winningGame(board, player);
 
-    return board
+  return board;
 }
 
 //AI play a pawn in board
 export function aiPlayPawn(board, player, seed) {
+  let box = Array.from(document.getElementsByClassName("box"));
 
-    let box = Array.from(document.getElementsByClassName('box'))
+  // Get random index between 0 and 8
+  let emptyGrid = box.filter(box => box.innerText === "");
 
-    // Get random index between 0 and 8
-    let emptyGrid = box.filter(box => box.innerText === "");
+  let randIndex = Math.floor(seed * emptyGrid.length);
 
-    let randIndex = Math.floor(seed * emptyGrid.length);
+  // Get coordinates based on the random index
+  let coord = getPosition(randIndex);
 
-    // Get coordinates based on the random index
-    let coord = getPosition(randIndex);
+  // if (board[coord.row][coord.column] === "") {
+  //     console.log(board[coord.row][coord.column].innerText = player);
+  //     alert()
+  // }
 
+  // filter empty cases
+  // while (board[coord.row][coord.column] !== "") {
+  //     randIndex = Math.floor(seed() * 9);
+  //     coord = getPosition(randIndex);
+  //     break;
+  // }
 
-    if (board[coord.row][coord.column] === "") {
-        console.log(board[coord.row][coord.column].innerText = player);
-        alert()
-    }
+  const result = playPawn(board, randIndex, player);
 
-    console.log(board);
-    // filter empty cases
-    // while (board[coord.row][coord.column] !== "") {
-    //     randIndex = Math.floor(seed() * 9);
-    //     coord = getPosition(randIndex);
-    //     break;
-    // }
-
-
-    const result = playPawn(board, randIndex, player);
-
-    return result;
+  return result;
 }
