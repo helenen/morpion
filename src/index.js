ad7469017f9d52b310699b36bc485250d8f3d05d
helenen <helene.niassy@gmail.com>
2019-07-31 22:15:12 +0200

    nothng

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
-import { generateBoard, playPawn, aiPlayPawn } from './scripts/game2.js'
+import { generateBoard, playPawn, aiPlayPawn } from "./scripts/game2.js";
 import { winningGame } from "./scripts/winning2.js";
 
 function render(board) {
-    const boardContainer = document.createElement("DIV");
-    const newboardContainer = document.createTextNode("DIV");
-    boardContainer.appendChild(newboardContainer);
-    boardContainer.setAttribute("class", "container");
-    document.body.appendChild(boardContainer);
+  const boardContainer = document.createElement("DIV");
+  boardContainer.setAttribute("class", "container");
+  document.body.appendChild(boardContainer);
+  let elem = document.querySelector("body");
+  elem.parentNode.removeChild(elem);
 
-    for (let i = 0; i < board.length; i++) {
-        const column = document.createElement("div");
-        column.setAttribute("class", "column");
-        boardContainer.appendChild(column);
+  for (let i = 0; i < board.length; i++) {
+    const column = document.createElement("div");
+    column.setAttribute("class", "column");
+    boardContainer.appendChild(column);
 
-        for (let j = 0; j < board[i].length; j++) {
-            const box = document.createElement("div");
-            box.setAttribute("class", "box");
-            box.setAttribute("value", "null");
-            box.setAttribute("id", "col-" + i + ' row-' + j);
+    for (let j = 0; j < board[i].length; j++) {
+      const box = document.createElement("div");
+      box.setAttribute("class", "box");
+      box.setAttribute("value", "null");
+      box.setAttribute("id", "col-" + i + " row-" + j);
 
-            if (board[i][j]) {
-                box.innerHTML = board[i][j]
-            }
+      if (board[i][j]) {
+        box.innerHTML = board[i][j];
+      }
 
-            box.addEventListener('click', () => {
-                const newBoard = playPawn(board, i, j, "X")
-                render(newBoard)
-            });
+      box.addEventListener("click", () => {
+        board = playPawn(board, i, j, "X");
 
-            column.appendChild(box);
-        }
+        render(board);
+      });
+
+      column.appendChild(box);
     }
+  }
 }
 
-render(generateBoard())
+render(generateBoard());
